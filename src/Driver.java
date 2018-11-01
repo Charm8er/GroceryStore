e491581c1831fd9d4d1ce687088d7ac3745c0b4e
Josh <jluttmer@student.cccs.edu>
2018-11-01 09:47:48 -0600

    Update Driver and Item

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,23 @@
 
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Item beef = new Item(); // create item "beef"
+		System.out.println(beef.toString()); // output item "beef"
+		Item chicken = new Item("Chicken", 2011, 7.99, 16, 20 ); // create item Chicken
+		System.out.println(chicken.toString()); // output item "Chicken"
+		System.out.println(chicken.getPrice()); // get item price
+		chicken.setPrice(-5); // set item price
+		System.out.println(chicken.toString()); // output item "Chicken"
+		
+		printInventory (chicken); // call printInventory
 
+	} // end of main
+	
+	public static void printInventory(Item item)
+	{
+		System.out.println("Name: " + item.getName ( ) + " " + " Stock: " + item.getStock( ));
 	}
-
-}
+ 
+} // end of Driver class
--- src/Item.java
+++ src/Item.java
@@ -6,5 +6,125 @@ public class Item {
 	private int weight;
 	private int stock;
 	
+	public Item()
+	{
+		name = null;
+		itemNum = 0000;
+		price = 0.00;
+		weight = 0;
+		stock = -100;
+	}
 
-}
+	/**
+	 * @param name // item name
+	 * @param itemNum // item number or barcode
+	 * @param price // sales price of item
+	 * @param weight // item weight in oz
+	 * @param stock // amount of item in inventory
+	 */
+	public Item(String name, int itemNum, double price, int weight, int stock) {
+		super();
+		this.name = name;
+		this.itemNum = itemNum;
+		this.price = price;
+		this.weight = weight;
+		this.stock = stock;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", itemNum=" + itemNum + ", price=" + price + ", weight=" + weight + ", stock="
+				+ stock + "]";
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() 
+	{
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the itemNum
+	 */
+	public int getItemNum() 
+	{
+		return itemNum;
+	}
+
+	/**
+	 * @param itemNum the itemNum to set
+	 */
+	public void setItemNum(int itemNum) 
+	{
+		this.itemNum = itemNum;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() 
+	{
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) 
+	{
+		if (price <= 0)
+		{
+			System.out.println("Invalid price, must be greater than 0.");
+		}
+		else
+		{
+			this.price = price;
+		}
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() 
+	{
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(int weight) 
+	{
+		this.weight = weight;
+	}
+
+	/**
+	 * @return the stock
+	 */
+	public int getStock() 
+	{
+		return stock;
+	}
+
+	/**
+	 * @param stock the stock to set
+	 */
+	public void setStock(int stock) 
+	{
+		this.stock = stock;
+	}
+	
+} // end of Item class
