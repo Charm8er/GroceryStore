1f3069e9021d77aaaa5c9c2944e7160775fd6d12
Josh <jluttmer@student.cccs.edu>
2018-11-08 20:32:35 -0700

    Update Driver
    - moved array stuff to importList method
    - adjusted while loop for array
    - added while loop to next import / array loop
    - added if statement to manually add item to array list
    - adjusted header
    - added comment for index variable

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,5 @@
 // Joshua Luttmer, Marcos Del Toro, Omar Arias
-// Store Program Part 1 
+// Store Program Part 3 
 // 11/8/2018
 // Reads in text file with name, item number, price, weight and inventory level.
 
@@ -18,38 +18,13 @@ public class Driver {
 		input = new Scanner (System.in);
 		inFile = new java.io.File("items");
 		inputFile = new Scanner(inFile);
-		Item arrayObject = new Item (); // Declaring object of item type, used to fill array
-		Item object1 = new Item(); // Declaring Item object
+		
 		Item [] groceryArray = new Item [MAX]; // Declaring and  initializing  array of grocery items
-		int index = 0;
-		
-		if(!inFile.exists())
-		{
-			System.out.println ( "file not found" );
-			System.exit(-1);
-		}
-		
-		System.out.printf ( "%15s%10s%10s%10s%10s\n","Name", "Item #", "Price", "Weight", "Stock\n" );
-		
-		while(inputFile.hasNext())
-		{
-			String name = inputFile.next ( ); // name of item
-			int itemNum = inputFile.nextInt ( ); // item number/barcode
-			double price = inputFile.nextDouble ( ); // price of item
-			int weight = inputFile.nextInt ( ); // weight of item in oz
-			int stock = inputFile.nextInt ( ); // number of items in stock
-			System.out.printf ( "%15s%10s%10s%10s%10s\n",name, itemNum, price, weight, stock );
-			arrayObject = new Item (name, itemNum, price, weight, stock);
-			groceryArray [index] = arrayObject;
-			index ++;
-		}
-		
-		inputFile.close ();
-		
-		createObject (object1);
-		printData(object1);
+		importList(groceryArray);
 		printArray (groceryArray);
 		
+//		createObject (object1);
+//		printData(newItem);			
 //		Item beef = new Item(); // create item "beef"
 //		System.out.println(beef.toString()); // output item "beef"
 //		Item chicken = new Item("Chicken", 2011, 7.99, 16, 20 ); // create item Chicken
@@ -57,7 +32,6 @@ public class Driver {
 //		System.out.println(chicken.getPrice()); // get item price
 //		chicken.setPrice(-5); // set item price
 //		System.out.println(chicken.toString()); // output item "Chicken"
-//		
 //		printInventory (chicken); // call printInventory
 
 	} // end of main
@@ -69,13 +43,15 @@ public class Driver {
 	public static void printInventory(Item item)
 	{
 		System.out.println("Name: " + item.getName ( ) + " " + " Stock: " + item.getStock( ));
-	}
-	/*********************************************************
+	} // end printInventory
+	
+	/******************************************************************************
 	 * @author Omar, Josh, Marcos
 	 * This method receives an Item object and allows user to input data members
-	 * @param object1, Item class object (user will input data)
-	 */
-	public static void createObject(Item object1)
+	 * @param newItem, Item class object (user will input data)
+	 * @return newItem
+	 ******************************************************************************/
+	public static Item createObject(Item newItem)
 	{
 		String name; // Name of item
 		int itemNum; // Item number/bar code
@@ -85,30 +61,33 @@ public class Driver {
 		
 		System.out.println ( "\nEnter item name: " );
 		name = input.next ( );
-		object1.setName ( name );
+		newItem.setName ( name );
 		
 		System.out.println ( "Enter item #: " );
 		itemNum = input.nextInt ( );
-		object1.setItemNum ( itemNum );
+		newItem.setItemNum ( itemNum );
 		
 		System.out.println ( "Enter item price: " );
 		price = input.nextDouble ( );
-		object1.setPrice ( price );
+		newItem.setPrice ( price );
 		
 		System.out.println ( "Enter item weight: " );
 		weight = input.nextInt ( );
-		object1.setWeight ( weight );
+		newItem.setWeight ( weight );
 		
 		System.out.println ( "Enter quantity in stock: " );
 		stock = input.nextInt ( );
-		object1.setStock ( stock );
+		newItem.setStock ( stock );
 		
-	} // End objectInput
+		return newItem;
+		
+	} // end objectInput
+	
 	/*********************************************************************
 	 * @author Omar, Marcos, Josh
 	 * This method prints the data members from the object
 	 * @param object1, Item whose data will be printed
-	 */
+	 *********************************************************************/
 	public static void printData(Item object1)
 	{
 
@@ -121,20 +100,71 @@ public class Driver {
 		System.out.println ( "Price: " + object1.getPrice ( ));
 		System.out.println ( "Weight: " + object1.getWeight ( ));
 		System.out.println ( "Stock: " + object1.getStock ( ));
-	} // End printData
-	/*********************************************************************
+	} // end printData
+	
+	/******************************************************************************************************
 	 * @author Omar , Josh, Marcos
 	 * This method prints the array
 	 * @param groceryArray, array of grocery items (name, item number, price, weight(oz), amount in stock)
-	 */
+	 ******************************************************************************************************/
 	public static void printArray (Item [] groceryArray)
 	{
-		for (int index = 0; index < 11; index ++)
+		int arrayLength = groceryArray.length; // LCV for array length
+		for (int index = 0; index < arrayLength; index ++)
 		{
 			System.out.println ( groceryArray [index] );
-		}
-	} // End printArray
+		} // end FOR index loc
+	} // end printArray
 	
+	/******************************************************************************************************
+	 * @author Josh , Omar, Marcos
+	 * This method imports "item" list and allows for user to manually input item.
+	 * @param groceryArray, array of grocery items (name, item number, price, weight(oz), amount in stock)
+	 ******************************************************************************************************/
+	public static void importList(Item [] groceryArray) 
+	{
+		Item arrayObject = new Item (); // Declaring object of item type, used to fill array
+		Item newItem = new Item(); // Declaring Item object
+		int index = 0; // array location
+		char addItem = 'y'; // LCV
+		if(!inFile.exists())
+		{
+			System.out.println ( "file not found" );
+			System.exit(-1);
+		}
+		
+		System.out.printf ( "%15s%10s%10s%10s%10s\n","Name", "Item #", "Price", "Weight", "Stock\n" );
+		
+		while(addItem == 'y') // Author Josh
+		{			
+			while (inputFile.hasNext()) // Author Omar
+			{
+				String name = inputFile.next ( ); // name of item
+				int itemNum = inputFile.nextInt ( ); // item number/barcode
+				double price = inputFile.nextDouble ( ); // price of item
+				int weight = inputFile.nextInt ( ); // weight of item in oz
+				int stock = inputFile.nextInt ( ); // number of items in stock
+				System.out.printf ( "%15s%10s%10s%10s%10s\n",name, itemNum, price, weight, stock );
+				arrayObject = new Item (name, itemNum, price, weight, stock);
+				groceryArray [index] = arrayObject;
+				index ++;
+			} // end WHILE hasNext
+			
+			while (addItem == 'y') // Author Josh
+			{						
+				System.out.println("\nAdd item manually? (y or n): ");
+				addItem = input.next().charAt(0);
+				if (addItem == 'y')
+				{
+					groceryArray [index] = createObject (newItem);
+					index++;					
+				} // end IF addItem y
+			} //end 2nd WHILE addItem y
+
+		} // end WHILE addItem y
+
+		inputFile.close ();
+	}
 } // end of Driver class
 
 
